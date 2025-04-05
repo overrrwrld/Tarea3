acde4c61ba4dfd737aa5f5db1f024bf43dc6b1fa
Oliver Alejandro Martínez Guzmán <omartinezinf26@gmail.com>
2025-04-04 21:46:17 -0400

    Creación del main page

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ try {
     die('Error en la consulta a la base de datos: ' . $e->getMessage());
 }
 ?>
-
+<!-- Aquí se crea la main page-->
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -115,4 +115,5 @@ try {
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     </body>
+    <!-- Aquí acaba la main page-->
 </html>
